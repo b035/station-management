e2ae0f79c6830d9ac7f65cd051f7bd4b8095bf65
b035 <marlon@erler.cc>
2023-02-18 19:44:27 +0100

    added registry methods

---
+++ management.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import * as SDK from "@the-stations-project/sdk";
+
+async function main(subcommand: string, args: string[]) {
+	switch (subcommand) {
+		case "registry": return (await registry(args));
+		default: return new SDK.Result(SDK.ExitCodes.ErrNoCommand, undefined);
+	}
+}
+
+async function registry(args: string[]) {
+	const [subcommand, path, content] = args;
+
+	switch (subcommand) {
+		case "read": return (await SDK.Registry.read(path));
+		case "write": return (await SDK.Registry.write(path, content));
+		case "mkdir": return (await SDK.Registry.mkdir(path));
+		case "delete": return (await SDK.Registry.delete(path));
+		case "ls": return (await SDK.Registry.ls(path));
+		default: return new SDK.Result(SDK.ExitCodes.ErrNoCommand, undefined);
+	}
+}
+
+SDK.start_service(main, (result) => console.log(result.to_string()));
